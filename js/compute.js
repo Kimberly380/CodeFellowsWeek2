916addd344ee718a36f465d9b90027975e4ddbeb
Kim Feinberg <kimberly380@gmail.com>
2016-03-22 07:52:16 -0700

    almost final.  updated app.js to reflect text.content changes to html.  updated html to recieve those text changes.  problems 4 and 5 are still not passing test.  unable to link app.js to the compute.js file, so all references to functions re commented out.

--- js/compute.js
+++ js/compute.js
@@ -1,5 +1,5 @@
 
-//function for sum of two numbers: Problem One
+//function for sum of two numbers: PROBLEM 1.
 
 function sumTwo (num1, num2) {
   console.log("The sum of " + num1 + " and " + num2 + " is " + (num1 + num2));
@@ -9,7 +9,7 @@ function sumTwo (num1, num2) {
 exports.sumTwo = sumTwo;
 
 
-//function for multiplying two numbers: Problem Two
+//function for multiplying two numbers: PROBLEM 2.
 
 function multTwo (a, b) {
   console.log("The product of " + a + " and " + b + " is "+ (a * b));
@@ -19,7 +19,7 @@ function multTwo (a, b) {
 exports.multTwo = multTwo;
 
 
-//function for summing and multiplying three numbers: Problem Three
+//function for summing and multiplying three numbers: PROBLEM 3.
 
 function sumAndMult (a, b, c) {
   console.log( a + " and " + b + " and " + c + " sum to "+(a+b+c));
@@ -34,7 +34,7 @@ function sumAndMult (a, b, c) {
 exports.sumAndMult = sumAndMult;
 
 
-//function for adding three items in an array. Problem Four.
+//function for adding three items in an array. PROBLEM 4.
 
 function sumArray(myArray) {
 
@@ -42,6 +42,7 @@ function sumArray(myArray) {
       var sumResults = 0;
       sumResults += myArray[i];
 
+      console.table(myArray);
       console.log(myArray[0]+", "+myArray[1]+ ", " + myArray[2] + " was passed in as an array of numbers, and " + sumResults + " is their sum.");
 
       return sumResults;
@@ -51,7 +52,7 @@ function sumArray(myArray) {
 exports.sumArray = sumArray;
 
 
-//function for multiplying three numbers in an array.  Problem Five.
+//function for multiplying three numbers in an array.  PROBLEM 5.
 
 function multArray(myArray2) {
 
@@ -60,7 +61,9 @@ function multArray(myArray2) {
     var multResults = 0;
     multResults *= myArray2[i];
 
+    console.table(myArray2);
     console.log("the numbers "+ myArray2[0] + ", " + myArray2[1] +", "+myArray[3] + " have a product of " + multResults);
+
 
     return multResults;
     }
